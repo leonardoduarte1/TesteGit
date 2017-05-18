efa1b6b72f036641666ae7f20b8d0d5b2aaac8a9
Lucas Emerim Marques <lucas.emerim@sinprors.org.br>
2017-05-18 16:20:17 -0300

    teste lucas
    aaaa

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        agora vai
+        teste lucas
         <?php
         // put your code here
         ?>
